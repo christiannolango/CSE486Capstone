4fd3c9994992faae75d9c84b9f19b3c3bd36de35
Camrie Madsen <camrie.madsen@gmail.com>
2019-11-06 13:08:50 -0700

    adding rcode

---
+++ django/django_project/examples/rcode/meta_test.R
@@ -0,0 +1,43 @@
+#! C:\\Users\\camri\\OneDrive\\Desktop\\capstone\\CSE486Capstone\Rscript
+suppressMessages({library(dplyr)
+library(meta)
+library(readxl)
+library(robumeta)
+library(grid)
+library(irr)})
+library('RPostgreSQL')
+library(forestplot)
+
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "PostgreSQL Unicode(x64)",
+                      Server   = "localhost",
+                      Database = "capstone_database",
+                      UID      = "research_database",
+                      PWD      = "capstone!",
+                      Port     = 5432)
+
+meta <- dbGetQuery(con, "SELECT * from examples_snp_entry;")
+meta$SNP_id <- as.character(meta$SNP_id)
+meta$SNP_id <- as.factor(meta$SNP_id)
+meta <- meta %>%
+  mutate(eff_se = sqrt(EFF_var))
+
+#d_to_g <- function(eff_d, n1, n2){
+#  eff_d * (1 - (3 / (4 * (n1 + n2) - 9)))
+#}
+#meta <- meta %>%
+#  mutate(eff_g = d_to_g(eff_size, cleft_n, compare_n))
+
+con_m <- robu(eff_se ~ 1, meta, studynum = SNP_id, var.eff.size = EFF_var, rho = 0.8, small = TRUE)
+con_m
+
+
+
+png(file='./forestplot.png')
+forest.robu(con_m, es.lab = "SNP_id", study.lab = "entry_id",
+            "Effect Size" = eff_se)
+dev.off()
+
+dbDisconnect(con)
+
+
