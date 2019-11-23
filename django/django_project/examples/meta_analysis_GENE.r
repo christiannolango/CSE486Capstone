5839d076dbdc94719aeca99ba30a3b0886e70056
mpredtec <mpredtec@asu.edu>
2019-11-23 16:14:28 -0700

    meta-analysis

--- django/django_project/examples/meta_analysis_GENE.r
+++ django/django_project/examples/meta_analysis_GENE.r
@@ -19,10 +19,14 @@ meta_SNP$SNP_id <- as.character(meta_SNP$SNP_id)
 meta_SNP$SNP_id <- as.factor(meta_SNP$SNP_id)
 meta_SNP$SNP_id <- as.character(meta_SNP$study_id)
 meta_SNP$SNP_id <- as.factor(meta_SNP$study_id)
+
+meta_SNP$author <- as.character(meta_SNP$author)
+meta_SNP$year <- as.character(meta_SNP$year)
+author_year = paste(author, year)
 #meta <- meta_SNP %>%
 #  mutate(eff_se = sqrt(EFF_var))
 
-result <- robu(EFF_score ~ 1, EFF_var, studynum = study_id, var.eff.size = EFF_var, rho = 0.8, small = TRUE)
+result <- robu(EFF_score ~ 1, meta_SNP, studynum = study_id, var.eff.size = EFF_var, rho = 0.8, small = TRUE)
 result
 
 png(file='./examples/static/plots/forestplot.png')
