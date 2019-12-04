bd38738bb8c87cede4f17f50bb081e320545a19d
Camrie Madsen <camrie.madsen@gmail.com>
2019-12-04 14:12:49 -0700

    refactor meta_analysis_snp.r

--- django/django_project/examples/meta_analysis_SNP.r
+++ django/django_project/examples/meta_analysis_SNP.r
@@ -20,11 +20,11 @@ meta_SNP$SNP_id <- as.factor(meta_SNP$SNP_id)
 meta <- meta_SNP %>%
   mutate(eff_se = sqrt(EFF_var))
 
-con_m <- robu(eff_se ~ 1, meta, studynum = SNP_id, var.eff.size = EFF_var, rho = 0.8, small = TRUE)
-con_m
+result <- robu(eff_se ~ 1, meta, studynum = SNP_id, var.eff.size = EFF_var, rho = 0.8, small = TRUE)
+result
 
 png(file='./examples/static/plots/forestplot.png')
-forest.robu(con_m, es.lab = "SNP_id", study.lab = "entryId",
+forest.robu(result, es.lab = "SNP_id", study.lab = "entryId",
             "Effect Size" = eff_se)
 dev.off()
 #}
